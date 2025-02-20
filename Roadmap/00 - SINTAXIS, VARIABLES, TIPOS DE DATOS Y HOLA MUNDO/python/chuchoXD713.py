19bf7bb76c1229b37d053e769f66d49ba3ebc63f
chuchoXD713 <chucho.xd713@gmail.com>
2025-02-20 14:58:53 -0600

    Create chuchoXD713.py
    es el primer acercamiento que tengo a la programacion, me ha costado un poco pero aquio esta

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/chuchoXD713.py
@@ -0,0 +1,32 @@
+# https://python.org/
+# comentarios de una sola linea
+""""
+comentarios
+en varias 
+lineas
+"""
+
+''' 
+comentario 
+de varias 
+lineas de texto
+'''
+
+my_variable = "variable_a"
+my_bariable = "variable_b"
+my_bariable = "variable_c"
+
+MY_CONSTANT = "mi constante"
+
+my_int = 1
+my_int = 2
+my_float = 1.5
+my_float = 0.5
+
+my_bool = False
+my_bool = True
+my_string = "mi cadena de texto"
+my_other_string = "mi otra cadena de texto"
+
+print ("¡Hola python!")
+
